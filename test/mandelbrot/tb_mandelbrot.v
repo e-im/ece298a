961cab108c86ce59ee7b49ba3b651e70157dc845
Evan McCarthy <emccarth@uwaterloo.ca>
2025-07-06 23:47:30 -0400

    make tests modular

--- test/mandelbrot/tb_mandelbrot.v
+++ test/mandelbrot/tb_mandelbrot.v
@@ -4,12 +4,12 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
+module tb_mandelbrot ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
+    $dumpvars(0, tb_mandelbrot);
     #1;
   end
 
